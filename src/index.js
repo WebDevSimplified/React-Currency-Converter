3acac7915f16fe341b1e15097354fb8edc0985ca
Hari <manjaka.rajaonson@gmail.com>
2022-07-26 22:10:58 +0300

    update code to react 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*
+    Application entry point
+*/
+
+// In react >= 18, first tell where is the root of the app
+const root = createRoot(document.getElementById("root"));
+
+// Render the app in strict mode
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
